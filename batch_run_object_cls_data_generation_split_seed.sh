2b765729d7849b428b83e65434db5046d81188cf
tttyuntian <tttyuntian@hotmail.com>
2023-04-09 16:52:43 -0400

    added data generation shell script

---
+++ batch_run_object_cls_data_generation_split_seed.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while getopts s:i: flag
+do
+    case "${flag}" in
+        s) seed=${OPTARG};;
+        i) split_id=${OPTARG};;
+    esac
+done
+
+level_list=( "PutNextLocal" )
+
+for level_name in "${level_list[@]}"
+do
+    echo BabyAI-${level_name}-v0-split-${split_id}
+    python ./scripts/make_agent_demos.py --env BabyAI-${level_name}-v0 \
+        --demos ../data/put_next_local/BabyAI-${level_name}-v0-split-${split_id} \
+        --episodes 2500 \
+        --valid-episodes 500 \
+        --seed $((${seed} * 10000000))
+done
+
