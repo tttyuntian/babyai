d0b11582f054a8a4abfa8fbc33d484c1a5eb807d
Zilai Zeng <zilai_zeng@brown.edu>
2023-06-05 23:35:01 -0400

    rule probing data generation

---
+++ batch_run_data_generation_rule_probing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while getopts s:i:l: flag
+do
+    case "${flag}" in
+        s) seed=${OPTARG};;
+        i) split_id=${OPTARG};;
+        l) level_name=${OPTARG};;
+    esac
+done
+
+echo BabyAI-${level_name}-v0-seed-${seed}
+python ./scripts/make_agent_demos_rule_probing.py --env BabyAI-${level_name}-v0 \
+    --demos ../data/rule_probing/${level_name}/seed-${seed}-split-${split_id} \
+    --episodes 10 \
+    --valid-episodes 5 \
+    --seed $((${seed} * 1000000))
