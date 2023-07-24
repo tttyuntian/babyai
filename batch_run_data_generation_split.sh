dbcdd9aa663afcf03ae331522394374fcbf31b69
tttyuntian <tttyuntian@hotmail.com>
2023-07-24 17:00:43 -0400

    added demos argument in batch generation script

--- batch_run_data_generation_split.sh
+++ batch_run_data_generation_split.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-while getopts s:i:l: flag
+while getopts s:i:l:d: flag
 do
     case "${flag}" in
         s) seed=${OPTARG};;
         i) split_id=${OPTARG};;
         l) level_name=${OPTARG};;
+        d) demos=${OPTARG};;
     esac
 done
 
 echo BabyAI-${level_name}-v0-split-${split_id}
 python ./scripts/make_agent_demos.py --env BabyAI-${level_name}-v0 \
-    --demos ../data/${level_name}/BabyAI-${level_name}-v0-split-${split_id} \
+    --demos ${demos} \
     --episodes 2500 \
     --valid-episodes 500 \
-    --seed $((${seed} * 10000000))
+    --seed $((${seed} * 1000000))
+
+#--demos ../data/${level_name}/BabyAI-${level_name}-v0-split-${split_id} \
